2ae1f41ef8fd6059d95472fa96b428da03fbcdca
klickers <bluebirid228@gmail.com>
2024-01-22 10:43:01 -0800

    add /update form and route

--- index.js
+++ index.js
@@ -18,15 +18,34 @@ app.get("/update-cobj", async (req, res) => {
 		title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
 	})
 })
-// * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post("/update", async (req, res) => {
+	const update = {
+		properties: {
+			name: req.body.name,
+			title: req.body.title,
+			role: req.body.role,
+		},
+	}
 
-// * Code for Route 3 goes here
+	const createObject = `https://api.hubapi.com/crm/v3/objects/2-22844203`
+	const headers = {
+		Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+		"Content-Type": "application/json",
+	}
+
+	try {
+		await axios.post(createObject, update, { headers })
+		res.redirect("back")
+	} catch (err) {
+		console.error(err)
+	}
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
